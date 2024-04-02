271698eba076f437b2a6c3d0ee34a5960646777b
Gsilvaol <gustavosilva1994_@hotmail.com>
2024-04-02 15:46:24 -0300

    Listagem de pedidos

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -11,8 +11,15 @@ import {
 
 import { OrderTableFilters } from './order-table-filters'
 import { OrderTableRow } from './order-table-row'
+import { getOrders } from '@/api/get-orders'
+import { useQuery } from '@tanstack/react-query'
 
 export function Orders() {
+  const { data: result } = useQuery({
+    queryKey: ['orders'],
+    queryFn: getOrders,
+  })
+
   return (
     <>
       <Helmet title="Pedidos" />
@@ -21,6 +28,7 @@ export function Orders() {
         <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
         <div className="space-y-2.5">
           <OrderTableFilters />
+          
           <div className="rounded-md border">
             <Table>
               <TableHeader>
@@ -36,9 +44,10 @@ export function Orders() {
                 </TableRow>
               </TableHeader>
               <TableBody>
-                {Array.from({ length: 10 }).map((_, i) => {
-                  return <OrderTableRow key={i} />
-                })}
+                {result &&
+                  result.orders.map((order) => {
+                    return <OrderTableRow key={order.orderId} order={order} />
+                  })}
               </TableBody>
             </Table>
           </div>
